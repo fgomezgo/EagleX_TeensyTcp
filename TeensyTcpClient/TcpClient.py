debc356a3c14b78ed5921338c55db895ef66b187
Fabian Gomez <a01209914@itesm.mx>
2017-07-07 22:40:53 -0500

    Modified client to test 10-DOF server responses

--- TeensyTcpClient/TcpClient.py
+++ TeensyTcpClient/TcpClient.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python
 
 import socket
+import time
 
 
 TCP_IP = '192.168.0.102'
 TCP_PORT = 80
-BUFFER_SIZE = 1024
-MESSAGE = "Hello world!"
+BUFFER_SIZE = 100
+#MESSAGE = "Hello world!"
 
 i = 1
-
+#Do an update of 10-DOF data each second
 while 1:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((TCP_IP, TCP_PORT))
-	s.send(MESSAGE)
-	data = s.recv(BUFFER_SIZE)
+	s.send('1010')
+	data_gyro = s.recv(BUFFER_SIZE)
 	s.close()
-
-	print "received data: ",i," ",data
+	
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send('1011')
+	data_accel = s.recv(BUFFER_SIZE)
+	s.close()
+	
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((TCP_IP, TCP_PORT))
+	s.send('1012')
+	data_magn = s.recv(BUFFER_SIZE)
+	s.close()
+	
+	print "received response: ",i
+	print "Gyroscope: ",			data_gyro
+	print "Accelerometer: ",	data_accel
+	print "Magnetometer: ",		data_magn
 	i = i+1
+	time.sleep(1)

599491e39bbe4afdd7bd2d16f2d02450ce3bda54
Luis Montoya <montoyacle@hotmail.com>
2018-02-11 11:37:20 -0600

    Added skeleton structure for library

---
+++ TeensyTcpServer/src/Imu/Imu.cpp
@@ -0,0 +1,11 @@
+/*
+  Eagle X's rover IMU library Imu.h - Accelerometer library
+*/
+#include "Arduino.h"
+#include "Imu.h"
+
+
+
+Imu::Imu(){
+    
+}
---
+++ TeensyTcpServer/src/Imu/Imu.h
@@ -0,0 +1,17 @@
+/*
+  Eagle X's rover IMU library Imu.h - Accelerometer library
+*/
+#ifndef Imu_h
+#define Imu_h
+
+#include "Arduino.h"
+
+class Imu{
+    public:
+        Imu();
+
+    private:
+
+};
+
+#endif

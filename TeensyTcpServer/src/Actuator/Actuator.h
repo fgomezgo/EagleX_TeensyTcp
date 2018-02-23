61a0caac92e5820aebff6f776af91f5298037e2f
Luis Montoya <montoyacle@hotmail.com>
2018-02-23 02:06:18 -0600

    Changed meth/func definitions, added  arm defs, cool defs

--- TeensyTcpServer/src/Actuator/Actuator.h
+++ TeensyTcpServer/src/Actuator/Actuator.h
@@ -12,24 +12,47 @@ class Actuator{
     Actuator(char reset, char error);
 
     /* -------------- Pololu Controllers -------------- */
-    // Utility methods/functions
-    void motorConfigureAndReset();
-    int motorReadByte();
-    unsigned int motorGetVariable(unsigned char variableID, unsigned char device);
-    
-    // Methods/functions for multi-device actions
-    void motorSetAllSpeed(int speedLeft, int speedRight);
-    unsigned int motorGetAvgVoltage();
-    unsigned int motorGetAvgTemp();
-    bool motorGetError();
-    void motorExitSafeStart();
+    // Generic methods/functions required for controlllers comms
 
-    // Methdos/functions for unique devices 
-    unsigned int motorGetVoltage(unsigned char device);
-    unsigned int motorGetTemp(unsigned char device);
-    void motorSetSpeed(int speed, unsigned char device);
+    void controllerConfigureReset();
+    int controllerReadByte();
+    unsigned int controllerGetVariable(unsigned char variableID, unsigned char device);
     
+    /* -------------- Drive System -------------- */
+    // Methods/fucntions that allow the user to set drive system speed/ get board information
+
+    // Multidevice instructions
+    void driveSetAllSpeed(int speedLeft, int speedRight);
+    unsigned int driveGetAvgVoltage();
+    unsigned int driveGetAvgTemp();
+    bool driveGetError();
+    void driveExitSafeStart();
+
+    // Unique device instructions
+    unsigned int driveGetVoltage(unsigned char device);
+    unsigned int driveGetTemp(unsigned char device);
+    void driveSetSpeed(int speed, unsigned char device);
     
+      
+    /* -------------- Arm motor controllers -------------- */
+    //TODO: will a delay work good enough?
+    void baseRotate(bool heading);
+
+    //TODO: Main idea is to use a closed loop system for shoulder position
+    void shoulderRotate(int position);
+    void elbowRotate(int position);
+
+    /* -------------- Gripper motor controllers -------------- */
+    //TODO: Same as above // Controls the servo
+    void forearmRotate(int position);
+    //TODO: Same as above
+    void wristRotate(int position);
+    //TODO: Control it with the tiny wheel thingy
+    void gripperMove(bool direction);
+
+    /* -------------- Cooling System -------------- */
+    //TODO: Plan is to send byte with fans states encoded?
+    void coolingSet(char config);
 
   private:
     char _resetPin;
